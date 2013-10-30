7bcc096c1fb39ccbf0e8fc773e7373ed167b6d47
Jesse Piascik <jesse@piascik.net>
2013-10-30 07:53:04 -0500

    Add markdown task with template

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,9 +12,63 @@ module.exports = function(grunt) {
           cleanBowerDir: false
         }
       }
+    },
+
+    readme: {
+      project: "piascikj/imdone"
+    },
+
+    markdown: {
+      all: {
+        src: 'readme.md',
+        dest: 'index.html',
+        options: {
+          template: 'index.html.jst',
+          preCompile: function(src, context) {},
+          postCompile: function(src, context) {},
+          templateContext: {
+            title: "iMDone",
+            description: ""
+          },
+          markdownOptions: {
+            gfm: true
+          }
+        }
+      }
     }
+
   });
 
   grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-markdown');
+
+  grunt.registerTask('readme', 'Get readme file', function() {
+    grunt.config.requires('readme.project');
+    var done = this.async();
+    var https = require('https');
+    var fs = require('fs');
+    var marked = 
+    https.get("https://api.github.com/repos/" + grunt.config("readme.project") + "/readme", function(res) {
+      var data = '';
+
+      res.on('data', function (chunk){
+          data += chunk;
+      });
+
+      res.on('end',function(){
+          var obj = JSON.parse(data);
+          var md = new Buffer(obj.content, 'base64').toString('ascii')
+          fs.writeFile('readme.md', md, function (err) {
+            if (err) {
+              done(err);
+            }
+            grunt.log.writeln('saved readme.md');
+            done(true);
+          });
+      });
+    });
+  });
+
+  grunt.registerTask('default', ['bower:install', 'readme', 'markdown']);
 
 };
--- js/main.js
+++ js/main.js
@@ -1,32 +1,26 @@
-//the require library is configuring paths
 require.config({
-    baseUrl: "lib",
-    paths: {
-                //tries to load jQuery from Google's CDN first and falls back
-                //to load locally
-        "bootstrap": "bootstrap/js/bootstrap.min",
-        "jquery": ["http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min",
+  baseUrl: "lib",
+  paths: {
+    "bootstrap"  : "bootstrap/js/bootstrap.min",
+    "jquery"     : ["http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min",
                     "jquery/jquery"],
-        "underscore": "underscore/js/underscore-min",
-        "backbone": "backbone/js/backbone-min",
-        "marked": "marked/js/marked",
-        "app": "../js/app"
-    },
-    shim: {
-        // Bootstrap
-        "bootstrap": ["jquery"],
-        "backbone": {
-                        //loads dependencies first
-            deps: ["jquery", "underscore"],
-                        //custom export name, this would be lowercase otherwise
-            exports: "Backbone"
-        }
-    },
-        //how long the it tries to load a script before giving up, the default is 7
-    waitSeconds: 10
+    "underscore" : "underscore/js/underscore-min",
+    "backbone"   : "backbone/js/backbone-min",
+    "marked"     : "marked/js/marked",
+    "prism"    : "prismjs/js/prism.js",
+    "app"        : "../js/app"
+  },
+  shim: {
+    "bootstrap" : ["jquery"],
+    "backbone"  : {
+                    deps    : ["jquery", "underscore"],
+                    exports : "Backbone"
+                  }
+  },
+  waitSeconds: 10
 });
-//requiring the scripts in the first argument and then passing the library namespaces into a callback
-//you should be able to console log all of the callback arguments
-require(['jquery', 'underscore', 'backbone', 'bootstrap', 'app'], function(jquery, _, Backbone, Bootstrap, App){
+
+require(['jquery', 'underscore', 'backbone', 'bootstrap', 'app', 'prism'], function(jquery, _, Backbone, Bootstrap, App){
+    Prism.highlightAll();
     new App;
 });
